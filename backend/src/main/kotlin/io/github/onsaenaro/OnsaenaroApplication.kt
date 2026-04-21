35d22a59e82c9f2c82e71dd3a6c8c1865e18f340
Jimin Seo <aa090402@naver.com>
2026-04-22 08:20:31 +0900

    feat: Spring boot backend

---
+++ backend/src/main/kotlin/io/github/onsaenaro/OnsaenaroApplication.kt
@@ -0,0 +1,11 @@
+package io.github.onsaenaro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnsaenaroApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnsaenaroApplication>(*args)
+}
---
+++ backend/src/main/kotlin/io/github/onsaenaro/controller/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package io.github.onsaenaro.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun hello() = "Hello World"
+}
